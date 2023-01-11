b3fb5786a360edc157f3237c5f418e5775aa81ee
trainb0y <trainb0ydev@gmail.com>
2023-01-11 15:12:46 -0700

    Kotlinify gradle files

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+  kotlin("jvm") version "1.8.0"
+  java
+  alias(libs.plugins.quilt.loom)
+}
+group = property("maven_group")!!
+version = property("version")!!
+
+repositories {
+  maven("https://jitpack.io") // mixin extras
+}
+
+dependencies {
+  minecraft(libs.minecraft)
+  mappings(variantOf(libs.quilt.mappings) { classifier("intermediary-v2") })
+  modImplementation(libs.quilt.loader)
+  modImplementation(libs.quilted.fabric.api)
+  modImplementation(libs.quilt.kotlin)
+
+  implementation(libs.mixinextras)
+  include(libs.mixinextras)
+  annotationProcessor(libs.mixinextras)
+}
+
+tasks {
+
+  processResources {
+    inputs.property("version", project.version)
+    filesMatching("quilt.mod.json") {
+      expand(mutableMapOf("version" to project.version))
+    }
+  }
+
+  jar {
+    from("LICENSE")
+  }
+
+  compileKotlin {
+    kotlinOptions.jvmTarget = "17"
+  }
+}
+
+java {
+  withSourcesJar()
+}
